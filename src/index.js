8673b1c0cd74e15f45f2b462f4d76212c1ae9b38
luongductran371 <luong.tran@hope.edu>
2023-01-02 15:57:31 -0500

    Hi

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import Contact from "./pages/Contact";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
